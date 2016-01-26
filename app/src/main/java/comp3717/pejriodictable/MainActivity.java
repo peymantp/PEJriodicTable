426f860e757907a7af44593942f2952de32f40b0
echlau <ericlau96@hotmail.com>
2016-01-26 14:18:53 -0800

    immersive fullscreen added.

--- app/src/main/java/comp3717/pejriodictable/MainActivity.java
+++ app/src/main/java/comp3717/pejriodictable/MainActivity.java
@@ -2,12 +2,27 @@ package comp3717.pejriodictable;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
-
+    View decorView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        decorView = (View)getWindow().getDecorView();
+    }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
     }
 }
