ba1d3ba64c20293e759d028e247cf1865dabfac2
peymantp <peymantparsa@gmail.com>
2016-01-30 20:56:49 -0800

    Made onClick for elements(no functionality)

--- app/src/main/java/comp3717/pejriodictable/MainActivity.java
+++ app/src/main/java/comp3717/pejriodictable/MainActivity.java
@@ -1,9 +1,14 @@
 package comp3717.pejriodictable;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static android.app.PendingIntent.getActivity;
 
 public class MainActivity extends AppCompatActivity {
     View decorView;
@@ -24,5 +29,21 @@ public class MainActivity extends AppCompatActivity {
                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+    }
+
+    public void onClick(View view){
+        //test function
+        Button element = (Button)view;
+        Button replace = (Button)findViewById(R.id.big1);
+
+        int buttonId = element.getId();
+
+        Context context = getApplicationContext();
+        //CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+        //Toast toast = Toast.makeText(context, String.valueOf(buttonId), duration);
+        //toast.show();
+        Toast.makeText(context, String.valueOf(buttonId), duration).show();
     }
 }
